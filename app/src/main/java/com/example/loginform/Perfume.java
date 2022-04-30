337a1623575fb2be21b0ee9aa70ae62d5dfcfdc0
aBdulAh220 <bitm-f19-041@superior.edu.pk>
2022-04-30 21:58:47 +0500

    Perfumes List Updated

--- app/src/main/java/com/example/loginform/Perfume.java
+++ app/src/main/java/com/example/loginform/Perfume.java
@@ -12,6 +12,15 @@ public class Perfume extends AppCompatActivity {
     ImageButton btnperfumes;
     ImageButton butper;
     ImageButton btc;
+    ImageButton bnc;
+    ImageButton bpc;
+    ImageButton byc;
+    ImageButton bhc;
+    ImageButton bfc;
+    ImageButton buc;
+    ImageButton bqc;
+    ImageButton brc;
+    ImageButton boc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +53,56 @@ btc.setOnClickListener(new View.OnClickListener() {
         startActivity(intent);
     }
 });
+bnc=(ImageButton) findViewById(R.id.per_g);
+bnc.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Intent intent=new Intent(Perfume.this,payment.class);
+        startActivity(intent);
+    }
+});
+bpc=(ImageButton) findViewById(R.id.per_c);
+        bpc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Perfume.this,payment.class);
+                startActivity(intent);
+            }
+        });
+
+byc=(ImageButton) findViewById(R.id.per_a);
+        byc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Perfume.this,payment.class);
+                startActivity(intent);
+            }
+        });
+        bhc=(ImageButton) findViewById(R.id.per_i);
+        bhc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Perfume.this,payment.class);
+                startActivity(intent);
+            }
+        });
+bfc=(ImageButton) findViewById(R.id.per_i);
+        bfc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Perfume.this,payment.class);
+                startActivity(intent);
+            }
+        });
+        buc=(ImageButton) findViewById(R.id.per_y);
+        buc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Perfume.this,payment.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
