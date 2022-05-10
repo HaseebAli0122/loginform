495a9b9c9000ce026d41945dd256b43974139a7a
Muhammad Haseeb Ali <98651031+HaseebAli0122@users.noreply.github.com>
2022-05-10 19:43:07 +0500

    updated project42

--- app/src/main/java/com/example/loginform/bottomnavigation.java
+++ app/src/main/java/com/example/loginform/bottomnavigation.java
@@ -4,12 +4,36 @@ import androidx.annotation.NonNull;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.WindowManager;
 
 public class bottomnavigation extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
+
+    @Override
+    public void onBackPressed(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(bottomnavigation.this);
+        builder.setMessage("Do you really want to exit?");
+        builder.setCancelable(true);
+        builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finishAffinity();
+            }
+        });
+        builder.setPositiveButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
